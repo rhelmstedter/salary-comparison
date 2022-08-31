5365ae90b69d2f652fbf0a96acc330a416b9b1f9
Russell Helmstedter <rhelmstedter@gmail.com>
2022-08-30 18:40:43 -0700

    finished barchart; updated description

--- salary_comparison.py
+++ salary_comparison.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 from dash import html
 
 TEAL = "#079A82"
@@ -18,7 +17,7 @@ rio_df = pd.read_csv(
     index_col="step",
 )
 santa_paula_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2020-2021-SantaPaula.csv",
+    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-SantaPaula.csv",
     index_col="step",
 )
 oxnard_union_df = pd.read_csv(
@@ -66,7 +65,7 @@ bachelors_30_units: pd.DataFrame = pd.concat(
         oxnard_elem_df["class 3"],
         pleasant_valley_df["class 3"],
         rio_df["class 2"],
-        santa_paula_df["class 3"],
+        santa_paula_df["class 2"],
         simi_valley_df["class 3"],
         ventura_df["class 2"],
     ],
@@ -96,7 +95,7 @@ bachelors_60_units: pd.DataFrame = pd.concat(
         oxnard_elem_df["class 5"],
         pleasant_valley_df["class 4"],
         rio_df["class 4"],
-        santa_paula_df["class 3"],
+        santa_paula_df["class 4"],
         simi_valley_df["class 5"],
         ventura_df["class 3"],
     ],
@@ -111,7 +110,7 @@ bachelors_75_units: pd.DataFrame = pd.concat(
         oxnard_elem_df["class 5"],
         pleasant_valley_df["class 4"],
         rio_df["class 4"],
-        santa_paula_df["class 3"],
+        santa_paula_df["class 5"],
         simi_valley_df["class 6"],
         ventura_df["class 4"],
     ],
@@ -229,7 +228,7 @@ def construct_bar_graph(
     degree: str,
     units: int,
 ) -> go.Figure:
-    """Calculates and displays the differnces in earnings across a 36 year teaching career.
+    """Calculates and displays the differences in earnings across a 36 year teaching career.
 
     :career_earnings: A dictionary with keys containing the district abbreviations and
         values of the career earnings for each district.
@@ -240,71 +239,49 @@ def construct_bar_graph(
 
     :returns: None
     """
+    career_earnings = dict(sorted(career_earnings.items(), key=lambda x: x[1]))
     career_premiums = {
         district: (monthly * 12 * 36) for district, monthly in monthly_premiums.items()
     }
-    sorted_career_earnings = dict(sorted(career_earnings.items(), key=lambda x: x[1]))
     career_earnings_deltas = {
         district: (
-            career_earnings[focus]
-            - career_premiums[focus]
-            - (career_earnings[district] - career_premiums[district]),
-            career_earnings[focus] - career_earnings[district],
+            (career_earnings[district] - career_premiums[district])
+            - (career_earnings[focus] - career_premiums[focus]),
+            career_earnings[district] - career_earnings[focus],
         )
-        for district in sorted_career_earnings.keys()
+        for district in career_earnings.keys()
     }
-    # Creating two subplots
-    fig = make_subplots(
-        rows=1,
-        cols=2,
-        specs=[[{}, {}]],
-        shared_xaxes=True,
-        shared_yaxes=False,
-        vertical_spacing=0.001,
-    )
+    hovertemplate = []
+    for delta in career_earnings_deltas.values():
+        if delta[0] == delta[1]:
+            hovertemplate.append(f"${delta[1]/1000:.0f}k difference with {focus}")
+        elif delta[1] < 0:
+            hovertemplate.append(
+                f"${delta[1]/1000:.0f}k to ${delta[0]/1000:.0f}k difference with {focus}"
+            )
+        else:
+            hovertemplate.append(
+                f"${delta[0]/1000:.0f}k to ${delta[1]/1000:.0f}k difference with {focus}"
+            )
     colors = [
         LIGHTGRAY,
     ] * len(DISTRICTS)
-    focus_index = list(sorted_career_earnings.keys()).index(focus)
+    focus_index = list(career_earnings.keys()).index(focus)
     colors[focus_index] = TEAL
-    # lifetime earnings
-    fig.append_trace(
+    fig = go.Figure()
+    fig.add_trace(
         go.Bar(
-            x=list(sorted_career_earnings.values()),
-            y=list(sorted_career_earnings.keys()),
+            x=list(career_earnings.values()),
+            y=list(career_earnings.keys()),
             marker_color=colors,
             name="",
             orientation="h",
-            hovertemplate=[f"${x/1_000_000:.2f}M" for x in sorted_career_earnings.values()],
+            hovertemplate=hovertemplate,
         ),
-        1,
-        1,
-    )
-    # lifetime earnings deltas
-    fig.append_trace(
-        go.Bar(
-            x=[abs(delta[0] - delta[1]) for delta in career_earnings_deltas.values()],
-            y=list(sorted_career_earnings.keys()),
-            base=[delta[1] for delta in career_earnings_deltas.values()],
-            marker=dict(
-                color=TEAL,
-                line=dict(
-                    color=TEAL,
-                    width=1,
-                ),
-            ),
-            name="",
-            orientation="h",
-            hovertemplate=[
-                f"${x[0]/1000:.0f}k to ${x[1]/1000:.0f}k"
-                for x in career_earnings_deltas.values()
-            ],
-        ),
-        1,
-        2,
     )
     fig.update_layout(
         yaxis=dict(
+            title="District",
             showgrid=False,
             showline=False,
             showticklabels=True,
@@ -314,42 +291,21 @@ def construct_bar_graph(
                 size=12,
                 color="rgb(82, 82, 82)",
             ),
-        ),
-        yaxis2=dict(
-            showgrid=False,
-            showline=True,
-            showticklabels=False,
-            linecolor="rgba(102, 102, 102, 0.8)",
-            linewidth=2,
-            domain=[0, 1],
         ),
         xaxis=dict(
             title="Lifetime Earnings",
-            zeroline=False,
-            showline=False,
-            showticklabels=True,
             tickprefix="$",
-            showgrid=True,
-            domain=[0, 0.45],
             tickfont=dict(
                 family="Lato",
                 size=12,
                 color="rgb(82, 82, 82)",
             ),
-        ),
-        xaxis2=dict(
-            title="Difference in Lifetime Earnings<br>Adjusted for Health Premiums",
-            zeroline=True,
             showline=False,
+            showgrid=False,
             showticklabels=True,
-            tickprefix="$",
-            showgrid=True,
-            domain=[0.50, 1],
-            tickfont=dict(
-                family="Lato",
-                size=12,
-                color="rgb(82, 82, 82)",
-            ),
+            linecolor="rgb(204, 204, 204)",
+            linewidth=2,
+            ticks="outside",
         ),
         autosize=True,
         margin=dict(
@@ -361,22 +317,6 @@ def construct_bar_graph(
         showlegend=False,
         plot_bgcolor="white",
     )
-    annotations = []
-    # # Title
-    # annotations.append(
-    #     dict(
-    #         xref="paper",
-    #         yref="paper",
-    #         x=0,
-    #         y=1,
-    #         xanchor="left",
-    #         yanchor="bottom",
-    #         text="Lifetime Earnings Compared by District",
-    #         font=dict(family="Lato", size=30, color="rgb(37,37,37)"),
-    #         showarrow=False,
-    #     )
-    # )
-    fig.update_layout(annotations=annotations)
     return fig
 
 
@@ -476,7 +416,7 @@ def construct_ploty_graph(
             xref="paper",
             yref="paper",
             x=0,
-            y=1.05,
+            y=1.10,
             xanchor="left",
             yanchor="bottom",
             text=f"{degree} Degree and {units} units",
@@ -487,6 +427,7 @@ def construct_ploty_graph(
     fig.update_layout(annotations=annotations)
     return fig
 
+
 def calc_career_diffs(
     career_earnings: dict[str, int],
     monthly_premiums: dict[str, int],
@@ -495,7 +436,7 @@ def calc_career_diffs(
     degree: str,
     units: int,
 ) -> list[html.P]:
-    """Calculates and displays the differnces in earnings across a 36 year teaching career.
+    """Calculates and displays the differences in earnings across a 36 year teaching career.
 
     :career_earnings: A dictionary with keys containing the district abbreviations and
         values of the career earnings for each district.
@@ -520,47 +461,22 @@ def calc_career_diffs(
     career_diffs = []
     career_diffs.append(
         html.P(
-            f"""This analysis assumes a teacher starts with a {degree} degree with {units} units and remains in {focus} for a 36 year career.\nThe {focus} teacher makes:\n"""
+            f"""This analysis assumes a teacher starts with a {degree} degree with {units} units and remains in {focus} for a 36 year career."""
         )
     )
-    for district, delta, insurance_delta in zip(
-        districts, career_earnings_deltas, career_earnings_deltas_insurance
-    ):
-        if district == focus:
-            continue
-        if insurance_delta < 0 and delta < 0:
-            if insurance_delta == delta:
-                career_diffs.append(
-                    html.P(f"🟥 ${abs(delta):,.0f} less than {district}.\n")
-                )
-            else:
-                career_diffs.append(
-                    html.P(
-                        f"🟥 ${abs(insurance_delta):,.0f} to ${abs(delta):,.0f} less than {district}.\n"
-                    )
-                )
-        elif insurance_delta >= 0 and delta <= 0:
-            career_diffs.append(
-                html.P(
-                    f"🟧 ${abs(delta):,.0f} less to ${abs(insurance_delta):,.0f} more than {district}.\n"
-                )
-            )
-        elif insurance_delta >= 0 and delta >= 0:
-            if insurance_delta == delta:
-                html.P(
-                    career_diffs.append(f"🟩 ${abs(delta):,.0f} more than {district}.\n")
-                )
-            else:
-                career_diffs.append(
-                    html.P(
-                        f"🟩 ${abs(delta):,.0f} to ${abs(insurance_delta):,.0f} more than {district}.\n"
-                    )
-                )
+    total_deltas = 0
+    for delta, insurance_delta in zip(career_earnings_deltas, career_earnings_deltas_insurance):
+        total_deltas += delta + insurance_delta
+    average_delta = total_deltas / (2 * len(districts))
+    career_diffs.append(
+        html.P(f"""Accounting for paying into health benefits, the {focus} teacher's lifetime earnings has an expected value of ${round(average_delta, -3):,.0f} compared to the other districts. To view the difference between each district, hover over the barchart.""")
+    )
     return career_diffs
+
 
 if __name__ == "__main__":
     df, degree, units = SALARY_PARAMETERS["Master's and 60 units"]
-    career_earnings = calc_career_earnings(df, DISTRICTS, "VUSD")
+    career_earnings = calc_career_earnings(df, DISTRICTS)
     fig = construct_bar_graph(
         career_earnings, MONTHLY_PREMIUMS, DISTRICTS, "VUSD", degree, units
     )
