781cabfb0b8eef284e6762a1807d7024fdc67179
Russell Helmstedter <rhelmstedter@gmail.com>
2022-09-24 15:57:34 -0700

    separated out tests for the functions and tests for the dataframe construction

---
+++ tests/test_for_typos.py
@@ -0,0 +1,65 @@
+import pytest
+from districts_data import DISTRICTS, SALARY_PARAMETERS
+from salary_comparison import calc_career_earnings
+
+
+@pytest.mark.parametrize(
+    "district, expected",
+    [
+        ("CVUSD", 2832936),
+        ("OUHSD", 3398180),
+        ("VUSD", 2640309),
+    ],
+)
+def test_earnings_BA_30(district, expected):
+    salary_data = SALARY_PARAMETERS["Bachelor's and 30 units"][0]
+    earnings = calc_career_earnings(salary_data, DISTRICTS)
+    actual = earnings[district]
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "district, expected",
+    [
+        ("OPUSD", 2834142),
+        ("OSD", 3201259),
+        ("OVSD", 3127890),
+    ],
+)
+def test_earnings_BA_45(district, expected):
+    salary_data = SALARY_PARAMETERS["Bachelor's and 45 units"][0]
+    earnings = calc_career_earnings(salary_data, DISTRICTS)
+    actual = earnings[district]
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "district, expected",
+    [
+        ("HESD", 3721746),
+        ("PVSD", 3166386 + 500 * 36),
+        ("SPUSD", 3606832),
+    ],
+)
+def test_earnings_MA_60(district, expected):
+    salary_data = SALARY_PARAMETERS["Master's and 60 units"][0]
+    earnings = calc_career_earnings(salary_data, DISTRICTS)
+    actual = earnings[district]
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "district, expected",
+    [
+        ("RSD", 3463724),
+        ("FUSD", 3392456),
+        ("SVUSD", 3102034),
+        ("MUSD", 3088200),
+        ("VUSD", 3097190),
+    ],
+)
+def test_earnings_MA_75(district, expected):
+    salary_data = SALARY_PARAMETERS["Master's and 75 units"][0]
+    earnings = calc_career_earnings(salary_data, DISTRICTS)
+    actual = earnings[district]
+    assert actual == expected
