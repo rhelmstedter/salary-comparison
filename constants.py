b78d4c5019debcc85d71df68f37c2ed76a8a09a3
Russell Helmstedter <rhelmstedter@gmail.com>
2022-09-28 20:51:15 -0700

    sorted keys

--- constants.py
+++ constants.py
@@ -1,18 +1,18 @@
 
 MONTHLY_PREMIUMS = {
+    "CVUSD": 160,
+    "FUSD": 300,
     "HESD": 0,
-    "OVSD": 350,
-    "OUHSD": 180,
+    "MUSD": 125,
+    "OPUSD": 400,
     "OSD": 130,
+    "OUHSD": 180,
+    "OVSD": 350,
     "PVSD": 200,
     "RSD": 200,
     "SPUSD": 250,
     "SVUSD": 250,
     "VUSD": 0,
-    "CVUSD": 160,
-    "FUSD": 300,
-    "OPUSD": 400,
-    "MUSD": 125,
 }
 
 DISTRICTS = list(MONTHLY_PREMIUMS.keys())
