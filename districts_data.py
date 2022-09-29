e0e48430b8f06c480370ff88ef65e50b3e900745
Russell Helmstedter <rhelmstedter@gmail.com>
2022-09-28 19:46:42 -0700

    dried up construction of the dataframes

--- districts_data.py
+++ districts_data.py
@@ -2,212 +2,187 @@ import pandas as pd
 
 from constants import DISTRICTS
 
-ventura_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-Ventura.csv",
-    index_col="step",
-)
-rio_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-Rio.csv",
-    index_col="step",
-)
-santa_paula_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-SantaPaula.csv",
-    index_col="step",
-)
-oxnard_union_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-OxnardU.csv",
-    index_col="step",
-)
-hueneme_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-Hueneme.csv",
-    index_col="step",
-)
-ocean_view_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-OceanView.csv",
-    index_col="step",
-)
-oxnard_elem_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-OxnardE.csv",
-    index_col="step",
-)
-pleasant_valley_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-PleasantValley.csv",
-    index_col="step",
-)
-simi_valley_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-SimiValley.csv",
-    index_col="step",
-)
-conejo_valley_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-ConejoValley.csv",
-    index_col="step",
-)
-fillmore_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-Fillmore.csv",
-    index_col="step",
-)
-oak_park_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-OakPark.csv",
-    index_col="step",
-)
-moorpark_df = pd.read_csv(
-    "https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/2022-2023-Moorpark.csv",
-    index_col="step",
-)
+
+def _create_dataframe(filename: str) -> pd.DataFrame:
+    """helper function to use in the dictionary comprehension"""
+    return pd.read_csv(
+        f"https://raw.githubusercontent.com/rhelmstedter/salary-comparison/main/data/{filename}",
+        index_col="step",
+    )
+
+
+labels_and_files = [
+    ("ventura_df", "2022-2023-Ventura.csv"),
+    ("rio_df", "2022-2023-Rio.csv"),
+    ("santa_paula_df", "2022-2023-SantaPaula.csv"),
+    ("oxnard_union_df", "2022-2023-OxnardU.csv"),
+    ("hueneme_df", "2022-2023-Hueneme.csv"),
+    ("ocean_view_df", "2022-2023-OceanView.csv"),
+    ("oxnard_elem_df", "2022-2023-OxnardE.csv"),
+    ("pleasant_valley_df", "2022-2023-PleasantValley.csv"),
+    ("simi_valley_df", "2022-2023-SimiValley.csv"),
+    ("conejo_valley_df", "2022-2023-ConejoValley.csv"),
+    ("fillmore_df", "2022-2023-Fillmore.csv"),
+    ("oak_park_df", "2022-2023-OakPark.csv"),
+    ("moorpark_df", "2022-2023-Moorpark.csv"),
+]
+
+
+salary_scales = {label: _create_dataframe(file) for label, file in labels_and_files}
 
 # NOTE: the order of the dataframes must match the order of the monthly premiums
 bachelors_30_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 3"],
-        ocean_view_df["class 3"],
-        oxnard_union_df["class 2"],
-        oxnard_elem_df["class 3"],
-        pleasant_valley_df["class 3"],
-        rio_df["class 2"],
-        santa_paula_df["class 2"],
-        simi_valley_df["class 3"],
-        ventura_df["class 2"],
-        conejo_valley_df["class 2"],
-        fillmore_df["class 3"],
-        oak_park_df["class 2"],
-        moorpark_df["class 3"],
+        salary_scales["hueneme_df"]["class 3"],
+        salary_scales["ocean_view_df"]["class 3"],
+        salary_scales["oxnard_union_df"]["class 2"],
+        salary_scales["oxnard_elem_df"]["class 3"],
+        salary_scales["pleasant_valley_df"]["class 3"],
+        salary_scales["rio_df"]["class 2"],
+        salary_scales["santa_paula_df"]["class 2"],
+        salary_scales["simi_valley_df"]["class 3"],
+        salary_scales["ventura_df"]["class 2"],
+        salary_scales["conejo_valley_df"]["class 2"],
+        salary_scales["fillmore_df"]["class 3"],
+        salary_scales["oak_park_df"]["class 2"],
+        salary_scales["moorpark_df"]["class 3"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 bachelors_45_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 4"],
-        ocean_view_df["class 4"],
-        oxnard_union_df["class 3"],
-        oxnard_elem_df["class 4"],
-        pleasant_valley_df["class 4"],
-        rio_df["class 3"],
-        santa_paula_df["class 3"],
-        simi_valley_df["class 4"],
-        ventura_df["class 3"],
-        conejo_valley_df["class 3"],
-        fillmore_df["class 4"],
-        oak_park_df["class 3"],
-        moorpark_df["class 4"],
+        salary_scales["hueneme_df"]["class 4"],
+        salary_scales["ocean_view_df"]["class 4"],
+        salary_scales["oxnard_union_df"]["class 3"],
+        salary_scales["oxnard_elem_df"]["class 4"],
+        salary_scales["pleasant_valley_df"]["class 4"],
+        salary_scales["rio_df"]["class 3"],
+        salary_scales["santa_paula_df"]["class 3"],
+        salary_scales["simi_valley_df"]["class 4"],
+        salary_scales["ventura_df"]["class 3"],
+        salary_scales["conejo_valley_df"]["class 3"],
+        salary_scales["fillmore_df"]["class 4"],
+        salary_scales["oak_park_df"]["class 3"],
+        salary_scales["moorpark_df"]["class 4"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 bachelors_60_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 5"],
-        ocean_view_df["class 5"],
-        oxnard_union_df["class 3"],
-        oxnard_elem_df["class 5"],
-        pleasant_valley_df["class 4"],
-        rio_df["class 4"],
-        santa_paula_df["class 4"],
-        simi_valley_df["class 5"],
-        ventura_df["class 3"],
-        conejo_valley_df["class 4"],
-        fillmore_df["class 4"],
-        oak_park_df["class 4"],
-        moorpark_df["class 5"],
+        salary_scales["hueneme_df"]["class 5"],
+        salary_scales["ocean_view_df"]["class 5"],
+        salary_scales["oxnard_union_df"]["class 3"],
+        salary_scales["oxnard_elem_df"]["class 5"],
+        salary_scales["pleasant_valley_df"]["class 4"],
+        salary_scales["rio_df"]["class 4"],
+        salary_scales["santa_paula_df"]["class 4"],
+        salary_scales["simi_valley_df"]["class 5"],
+        salary_scales["ventura_df"]["class 3"],
+        salary_scales["conejo_valley_df"]["class 4"],
+        salary_scales["fillmore_df"]["class 4"],
+        salary_scales["oak_park_df"]["class 4"],
+        salary_scales["moorpark_df"]["class 5"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 bachelors_75_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 5"],
-        ocean_view_df["class 5"],
-        oxnard_union_df["class 3"],
-        oxnard_elem_df["class 5"],
-        pleasant_valley_df["class 4"],
-        rio_df["class 4"],
-        santa_paula_df["class 5"],
-        simi_valley_df["class 6"],
-        ventura_df["class 4"],
-        conejo_valley_df["class 4"],
-        fillmore_df["class 5"],
-        oak_park_df["class 5"],
-        moorpark_df["class 5"],
+        salary_scales["hueneme_df"]["class 5"],
+        salary_scales["ocean_view_df"]["class 5"],
+        salary_scales["oxnard_union_df"]["class 3"],
+        salary_scales["oxnard_elem_df"]["class 5"],
+        salary_scales["pleasant_valley_df"]["class 4"],
+        salary_scales["rio_df"]["class 4"],
+        salary_scales["santa_paula_df"]["class 5"],
+        salary_scales["simi_valley_df"]["class 6"],
+        salary_scales["ventura_df"]["class 4"],
+        salary_scales["conejo_valley_df"]["class 4"],
+        salary_scales["fillmore_df"]["class 5"],
+        salary_scales["oak_park_df"]["class 5"],
+        salary_scales["moorpark_df"]["class 5"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 masters_30_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 4"],
-        ocean_view_df["class 4"],
-        oxnard_union_df["class 3"],
-        oxnard_elem_df["class 3"],
-        pleasant_valley_df["class 3"] + 500,
-        rio_df["class 5"],
-        santa_paula_df["class 5"],
-        simi_valley_df["class 6"],
-        ventura_df["class 3"],
-        conejo_valley_df["class 2"],
-        fillmore_df["class 6"],
-        oak_park_df["class 3"],
-        moorpark_df["class 6"],
+        salary_scales["hueneme_df"]["class 4"],
+        salary_scales["ocean_view_df"]["class 4"],
+        salary_scales["oxnard_union_df"]["class 3"],
+        salary_scales["oxnard_elem_df"]["class 3"],
+        salary_scales["pleasant_valley_df"]["class 3"] + 500,
+        salary_scales["rio_df"]["class 5"],
+        salary_scales["santa_paula_df"]["class 5"],
+        salary_scales["simi_valley_df"]["class 6"],
+        salary_scales["ventura_df"]["class 3"],
+        salary_scales["conejo_valley_df"]["class 2"],
+        salary_scales["fillmore_df"]["class 6"],
+        salary_scales["oak_park_df"]["class 3"],
+        salary_scales["moorpark_df"]["class 6"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 masters_45_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 5"],
-        ocean_view_df["class 5"],
-        oxnard_union_df["class 4"],
-        oxnard_elem_df["class 4"],
-        pleasant_valley_df["class 4"] + 500,
-        rio_df["class 5"],
-        santa_paula_df["class 5"],
-        simi_valley_df["class 6"],
-        ventura_df["class 3"],
-        conejo_valley_df["class 3"],
-        fillmore_df["class 6"],
-        oak_park_df["class 4"],
-        moorpark_df["class 6"],
+        salary_scales["hueneme_df"]["class 5"],
+        salary_scales["ocean_view_df"]["class 5"],
+        salary_scales["oxnard_union_df"]["class 4"],
+        salary_scales["oxnard_elem_df"]["class 4"],
+        salary_scales["pleasant_valley_df"]["class 4"] + 500,
+        salary_scales["rio_df"]["class 5"],
+        salary_scales["santa_paula_df"]["class 5"],
+        salary_scales["simi_valley_df"]["class 6"],
+        salary_scales["ventura_df"]["class 3"],
+        salary_scales["conejo_valley_df"]["class 3"],
+        salary_scales["fillmore_df"]["class 6"],
+        salary_scales["oak_park_df"]["class 4"],
+        salary_scales["moorpark_df"]["class 6"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 masters_60_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 5"],
-        ocean_view_df["class 5"],
-        oxnard_union_df["class 4"],
-        oxnard_elem_df["class 5"],
-        pleasant_valley_df["class 5"] + 500,
-        rio_df["class 5"],
-        santa_paula_df["class 5"],
-        simi_valley_df["class 6"],
-        ventura_df["class 4"],
-        conejo_valley_df["class 4"],
-        fillmore_df["class 6"],
-        oak_park_df["class 5"],
-        moorpark_df["class 6"],
+        salary_scales["hueneme_df"]["class 5"],
+        salary_scales["ocean_view_df"]["class 5"],
+        salary_scales["oxnard_union_df"]["class 4"],
+        salary_scales["oxnard_elem_df"]["class 5"],
+        salary_scales["pleasant_valley_df"]["class 5"] + 500,
+        salary_scales["rio_df"]["class 5"],
+        salary_scales["santa_paula_df"]["class 5"],
+        salary_scales["simi_valley_df"]["class 6"],
+        salary_scales["ventura_df"]["class 4"],
+        salary_scales["conejo_valley_df"]["class 4"],
+        salary_scales["fillmore_df"]["class 6"],
+        salary_scales["oak_park_df"]["class 5"],
+        salary_scales["moorpark_df"]["class 6"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 masters_75_units: pd.DataFrame = pd.concat(
     [
-        hueneme_df["class 5"],
-        ocean_view_df["class 5"],
-        oxnard_union_df["class 4"],
-        oxnard_elem_df["class 6"],
-        pleasant_valley_df["class 5"] + 500,
-        rio_df["class 5"],
-        santa_paula_df["class 5"],
-        simi_valley_df["class 6"],
-        ventura_df["class 5"],
-        conejo_valley_df["class 5"],
-        fillmore_df["class 6"],
-        oak_park_df["class 5"],
-        moorpark_df["class 6"],
+        salary_scales["hueneme_df"]["class 5"],
+        salary_scales["ocean_view_df"]["class 5"],
+        salary_scales["oxnard_union_df"]["class 4"],
+        salary_scales["oxnard_elem_df"]["class 6"],
+        salary_scales["pleasant_valley_df"]["class 5"] + 500,
+        salary_scales["rio_df"]["class 5"],
+        salary_scales["santa_paula_df"]["class 5"],
+        salary_scales["simi_valley_df"]["class 6"],
+        salary_scales["ventura_df"]["class 5"],
+        salary_scales["conejo_valley_df"]["class 5"],
+        salary_scales["fillmore_df"]["class 6"],
+        salary_scales["oak_park_df"]["class 5"],
+        salary_scales["moorpark_df"]["class 6"],
     ],
     axis=1,
     ignore_index=True,
-).rename(columns={i: DISTRICTS[i] for i in range(len(DISTRICTS))})
+).rename(columns={i: district for i, district in enumerate(DISTRICTS)})
 
 SALARY_PARAMETERS = {
     "Bachelor's and 30 units": (bachelors_30_units, "Bachelor's", 30),
