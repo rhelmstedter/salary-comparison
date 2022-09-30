9a380b69c38ebfafa876202f5a5bd54b733e5ab1
Russell Helmstedter <rhelmstedter@gmail.com>
2022-09-29 17:17:40 -0700

    condensed all tests into one using parametrize

--- tests/test_for_typos.py
+++ tests/test_for_typos.py
@@ -1,84 +1,30 @@
-from copy import deepcopy
-
 import pytest
 
 from districts_data import SALARY_PARAMETERS
-from salary import Salary
 
 
 @pytest.mark.parametrize(
-    "district, expected",
+    "salary, district, expected",
     [
-        ("CVUSD", 2832936),
-        ("OUHSD", 3398180),
-        ("VUSD", 2640309),
+        (SALARY_PARAMETERS["Bachelor's and 30 units"], "CVUSD", 2832936),
+        (SALARY_PARAMETERS["Bachelor's and 30 units"], "OUHSD", 3398180),
+        (SALARY_PARAMETERS["Bachelor's and 30 units"], "VUSD", 2640309),
+        (SALARY_PARAMETERS["Bachelor's and 45 units"], "OPUSD", 2834142),
+        (SALARY_PARAMETERS["Bachelor's and 45 units"], "OSD", 3201259),
+        (SALARY_PARAMETERS["Bachelor's and 45 units"], "OVSD", 3127890),
+        (SALARY_PARAMETERS["Master's and 60 units"], "HESD", 3721746),
+        (SALARY_PARAMETERS["Master's and 60 units"], "PVSD", 3166386 + 500 * 36),
+        (SALARY_PARAMETERS["Master's and 60 units"], "SPUSD", 3606832),
+        (SALARY_PARAMETERS["Master's and 75 units"], "RSD", 3463724),
+        (SALARY_PARAMETERS["Master's and 75 units"], "FUSD", 3392456),
+        (SALARY_PARAMETERS["Master's and 75 units"], "SVUSD", 3102034),
+        (SALARY_PARAMETERS["Master's and 75 units"], "MUSD", 3088200),
+        (SALARY_PARAMETERS["Master's and 75 units"], "VUSD", 3097190),
     ],
 )
-def test_earnings_BA_30(district, expected):
+def test_earnings(salary, district, expected):
     """Confirm that \"Bachelor's and 30 units\" dataframe is constructed
     acurately
     """
-    data, degree, units = SALARY_PARAMETERS["Bachelor's and 30 units"]
-    salary = Salary(deepcopy(data), degree, units)
-    earnings = salary.calc_career_earnings()
-    actual = earnings[district]
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "district, expected",
-    [
-        ("OPUSD", 2834142),
-        ("OSD", 3201259),
-        ("OVSD", 3127890),
-    ],
-)
-def test_earnings_BA_45(district, expected):
-    """Confirm that \"Bachelor's and 45 units\" dataframe is constructed
-    acurately
-    """
-    data, degree, units = SALARY_PARAMETERS["Bachelor's and 45 units"]
-    salary = Salary(deepcopy(data), degree, units)
-    earnings = salary.calc_career_earnings()
-    actual = earnings[district]
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "district, expected",
-    [
-        ("HESD", 3721746),
-        ("PVSD", 3166386 + 500 * 36),
-        ("SPUSD", 3606832),
-    ],
-)
-def test_earnings_MA_60(district, expected):
-    """Confirm that \"Master's and 60 units\" dataframe is constructed
-    acurately
-    """
-    data, degree, units = SALARY_PARAMETERS["Master's and 60 units"]
-    salary = Salary(deepcopy(data), degree, units)
-    earnings = salary.calc_career_earnings()
-    actual = earnings[district]
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "district, expected",
-    [
-        ("RSD", 3463724),
-        ("FUSD", 3392456),
-        ("SVUSD", 3102034),
-        ("MUSD", 3088200),
-        ("VUSD", 3097190),
-    ],
-)
-def test_earnings_MA_75(district, expected):
-    """Confirm that \"Master's and 75 units\" dataframe is constructed
-    acurately
-    """
-    data, degree, units = SALARY_PARAMETERS["Master's and 75 units"]
-    salary = Salary(deepcopy(data), degree, units)
-    earnings = salary.calc_career_earnings()
-    actual = earnings[district]
+    actual = salary.calc_career_earnings()[district]
     assert actual == expected
