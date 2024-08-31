484430413674d33b2e2b14219c928aca151acd2f
Russell Helmstedter <rhelmstedter@gmail.com>
2024-08-31 13:35:49 -0700

    updated SPUSD 24-25

--- districts_data.py
+++ districts_data.py
@@ -20,7 +20,7 @@ disticts_and_files: tuple[str] = (
     ("OVSD", "2024-2025-OceanView.csv"),
     ("PVSD", "2022-2023-PleasantValley.csv"),
     ("RSD", "2024-2025-Rio.csv"),
-    ("SPUSD", "2022-2023-SantaPaula.csv"),
+    ("SPUSD", "2024-2025-SantaPaula.csv"),
     ("SVUSD", "2022-2023-SimiValley.csv"),
     ("VUSD", "2024-2025-Ventura.csv"),
 )
